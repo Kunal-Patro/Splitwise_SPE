77bfac6265cef8b45d60ec38b787b7285d44505e
Kunal Sharma <kunal0199@gmail.com>
2022-04-27 02:52:35 +0530

    Basics and user model made

---
+++ src/main/java/com/spe/splitwise/SplitwiseApplication.java
@@ -0,0 +1,12 @@
+package com.spe.splitwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SplitwiseApplication {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SplitwiseApplication.class, args);
+    }
+}
---
+++ src/main/java/com/spe/splitwise/models/User.java
@@ -0,0 +1,73 @@
+package com.spe.splitwise.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Column(name = "userID", unique = true, nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userID;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Id
+    @Column(name = "email", length = 100, unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", length = 100, nullable = false)
+    private String password;
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(Integer userID, String name, String email, String password) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
